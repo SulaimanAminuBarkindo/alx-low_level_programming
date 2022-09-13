0128b8b9b3df771d54c065bd6c78c3676087d5fc
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-09-13 17:40:27 +0100

    Add file to print alphabets in small letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Prints the alphabet in lowercase.
  *
- * print_alphabet - print alphabets in small letter
- *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
