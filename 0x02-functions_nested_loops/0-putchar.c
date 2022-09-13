ca7a814cd77b9d03ce423aceb34fe2eb95ea56e4
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-09-13 16:57:31 +0100

    Add file to write _putchar using user defined header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
+ * _putchar - write contents of c  using 
+ * POSIX system call
  * Return: Always 0 for success
  */
 int main(void)
 {
-	printf("_putchar\n");
+	int[] str = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int arrLength = sizeof(str) / sizeof(int);
+	
+	for(int i = 0; i < arrLength; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
